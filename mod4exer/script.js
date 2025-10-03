8808709c5f6c89a46e5cf336beb5bbf1aff449a7
Gabriel Medina <gabriel.medina@doane.edu>
2025-10-02 20:59:06 -0500

    script to get the bacon ipsum is complete and works correctly + slight css tweaks

--- mod4exer/script.js
+++ mod4exer/script.js
@@ -1,5 +1,24 @@
 async function getMeat() {
     var api =  "https://baconipsum.com/api/";
-    var paragrahNum;
+    var paragrahNum = document.getElementById("meat_form").value;
     api = api + "?type=meat&paras=" + paragrahNum;
+
+    alert(api);
+
+    var meat = await fetch(api);
+    var meatJSON = await meat.json();
+
+    document.getElementById("raw_text").innerHTML = JSON.stringify(meatJSON);
+
+    var paragrahForm = "";
+
+    for (para in meatJSON) {
+        if(paragrahForm === "") {
+            paragrahForm += meatJSON[para] + "<br>";
+        } else {
+            paragrahForm += "<br>" + meatJSON[para] + "<br>";
+        }
+    }
+
+    document.getElementById("formatted_text").innerHTML = paragrahForm;
 }
